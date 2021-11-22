a28dc0d4c9dbeb1ef643823cd552a839b5720569
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2021-11-22 19:31:19 -0400

    Add ability to set placeholder and fixed service compatibility

--- lovelace-live-text-update-card.js
+++ lovelace-live-text-update-card.js
@@ -4,21 +4,29 @@ class LiveTextUpdate extends Polymer.Element {
     this._hass = hass;
     const domain = this._config.domain
     const service = this._config.service
+    const placeholder = this._config.placeholder
   }
 
   ready() {
     super.ready();
-    //this.$.textinput.addEventListener('click', ev => ev.stopPropagation());
     this.$.textinput.addEventListener('keyup', keyCode => keyCode);
+
+    var placeholder = this._config.placeholder
+      if (placeholder == undefined) {
+        this.$.textinput.placeholder = "Search"
+      }
+      else if (placeholder != undefined){
+        this.$.textinput.placeholder = this._config.placeholder
+      }
   }
 
   setConfig(config) {
-    if (!config.service) {
-      throw new Error('You need to define a service');
-    }
-    if (!config.domain) {
-      throw new Error('You need to define a domain');
-    }
+      if (!config.service) {
+        throw new Error('You need to define a service');
+      }
+      if (!config.domain) {
+        throw new Error('You need to define a domain');
+      }
     this._config = config;
   }
 
@@ -26,26 +34,24 @@ class LiveTextUpdate extends Polymer.Element {
     const newValue = globalThis.$.keyValue;
     const serviceData = {
       delay_ms: 0,
-      is_combination: false,
       message: newValue,
     };
     this._hass.callService(this._config.domain, this._config.service, serviceData);
+
   }
 
   keyPress(keyEvent) {
     globalThis.$.currentKey = keyEvent.key
     var preventCharacterRepeat = keyEvent.key
-
       if (keyEvent.key == "Backspace") {
         globalThis.$.keyValue = "KEY_BACKSPACE"
         this.callService()
       }
-
       else if (keyEvent.key == "Enter") {
         globalThis.$.keyValue = "KEY_RETURN"
         this.callService()
+        this.$.textinput.value = ""
       }
-
       else if (keyEvent.key == "Shift") {
         void 0
       }
@@ -86,8 +92,8 @@ class LiveTextUpdate extends Polymer.Element {
           type="[[mode]]"
           on-keydown="keyPress"
           on-input="textInput"
-          id="textinput"
           placeholder=""
+          id="textinput"
           <div prefix>$</div>
           <ha-icon-button id="clearButton" slot="suffix" on-click="clearText">
           <ha-icon icon="mdi:close"></ha-icon>
